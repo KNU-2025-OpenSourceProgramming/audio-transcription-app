a418e9164e845c866751a78f2899da81eb380de5
severin <6severin9@gmail.com>
2025-05-14 16:35:00 +0900

    改善 UI 标题及按钮样式

--- frontend/audio-client/src/App.js
+++ frontend/audio-client/src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Audio Transcription App
+          </Typography>
+          <Button color="inherit" style={{ backgroundColor: '#3f51b5', color: '#fff', margin: '0 10px' }}>Login</Button>
+        </Toolbar>
+      </AppBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
